bb3dcda558a95021872264d321700b7b3343ea3a
Erik Ylipää <erik.ylipaa@gmail.com>
2025-02-03 23:41:05 +0100

    Some minor improvements

--- train_single_node.py
+++ train_single_node.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import StratifiedKFold
 import sklearn.datasets
 from scipy.io.arff import loadarff
 import pandas as pd
+from tqdm import tqdm
  
 SEED = 1729 
 N_OUTER_FOLDS = 5
@@ -28,48 +29,6 @@ class TrainingConfig:
     reg_lambda: float = 0.0
     
 
-def make_splits(X,y, rng: np.random.Generator):
-    outer_seed = rng.integers(0, 2**32-1)
-    outer_split = StratifiedKFold(n_splits=N_OUTER_FOLDS, shuffle=True, random_state=outer_seed)
-    indices = np.arange(len(y))
-    for modelling_indices, test_indices in outer_split.split(indices, y=y):
-        inner_seed = rng.integers(0, 2**32-1)
-        inner_split = StratifiedKFold(n_splits=N_INNER_FOLDS, shuffle=True, random_state=inner_seed)
-        modelling_y = y.iloc[modelling_indices]
-        test_X = X.iloc[test_indices]
-        test_y = y.iloc[test_indices]
-        # N.b. splitting the indices here can lead to an easy to make mistake. The StratifiedKFold.split 
-        # method will _reindex_ the input, and return splits of that index, but since the input is an 
-        # index this is an easy thing to miss.
-        for train_meta_indices, dev_meta_indices in inner_split.split(modelling_indices, y=modelling_y):  
-            # This is where things can go wrong. The output from split is not a direct split 
-            # of the ingoing modelling indices, instead it is a split of the reindex version 
-            # of the input, so we need to index out the actual data point indices
-            train_indices = modelling_indices[train_meta_indices]
-            dev_indices = modelling_indices[dev_meta_indices]
-            train_X = X.iloc[train_indices]
-            train_y = y.iloc[train_indices]
-            dev_X = X.iloc[dev_indices]
-            dev_y = y.iloc[dev_indices]
-            assert set(train_indices).isdisjoint(dev_indices) and set(train_indices).isdisjoint(test_indices), "The training set overlaps the dev or test set"
-            yield {'train': {'X': train_X, 'y': train_y},
-                   'dev': {'X': dev_X, 'y': dev_y},
-                   'test': {'X': test_X, 'y': test_y}}
-            
-
-def train_model(dataset_split, training_config: TrainingConfig):
-    train_data = lgb.Dataset(dataset_split['train']['X'], label=dataset_split['train']['y'])
-    dev_data = lgb.Dataset(dataset_split['dev']['X'], label=dataset_split['dev']['y'])
-    test_X, test_y = dataset_split['test']['X'], dataset_split['test']['y']
-    
-    params = asdict(training_config)
-    params['verbose'] = 1
-    model = lgb.train(params, train_set=train_data, valid_sets=[dev_data])
-    predictions = model.predict(test_X)
-    roc_auc = roc_auc_score(test_y, predictions)
-    return roc_auc
-    #model = lgb.train(params, train_set=train_data, valid_sets=[(dataset_split['dev']['X'], dataset_split['dev']['y'])])
-    
 
 def load_heart_statlog():
     data, metadata = loadarff('BNG_heart-statlog.arff')
@@ -100,22 +59,77 @@ def load_heart_statlog():
 def load_data():
     X,y = load_heart_statlog()
     return X,y
+
+
+def make_splits(X,y, rng: np.random.Generator):
+    outer_seed = rng.integers(0, 2**32-1)
+    outer_split = StratifiedKFold(n_splits=N_OUTER_FOLDS, shuffle=True, random_state=outer_seed)
+    indices = np.arange(len(y))
+    for modelling_indices, test_indices in outer_split.split(indices, y=y):
+        inner_seed = rng.integers(0, 2**32-1)
+        inner_split = StratifiedKFold(n_splits=N_INNER_FOLDS, shuffle=True, random_state=inner_seed)
+        modelling_y = y.iloc[modelling_indices]
+        test_X = X.iloc[test_indices]
+        test_y = y.iloc[test_indices]
+        # N.b. splitting the indices here can lead to an easy to make mistake. The StratifiedKFold.split 
+        # method will _reindex_ the input, and return splits of that index, but since the input is an 
+        # index this is an easy thing to miss.
+        for train_meta_indices, dev_meta_indices in inner_split.split(modelling_indices, y=modelling_y):  
+            # This is where things can go wrong. The output from split is not a direct split 
+            # of the ingoing modelling indices, instead it is a split of the reindex version 
+            # of the input, so we need to index out the actual data point indices
+            train_indices = modelling_indices[train_meta_indices]
+            dev_indices = modelling_indices[dev_meta_indices]
+            train_X = X.iloc[train_indices]
+            train_y = y.iloc[train_indices]
+            dev_X = X.iloc[dev_indices]
+            dev_y = y.iloc[dev_indices]
+            assert set(train_indices).isdisjoint(dev_indices) and set(train_indices).isdisjoint(test_indices), "The training set overlaps the dev or test set"
+            yield {'train': {'X': train_X, 'y': train_y},
+                   'dev': {'X': dev_X, 'y': dev_y},
+                   'test': {'X': test_X, 'y': test_y}}
+            
+
+def train_model(dataset_split, training_config: TrainingConfig, seed=None):
+    train_data = lgb.Dataset(dataset_split['train']['X'], label=dataset_split['train']['y'])
+    dev_data = lgb.Dataset(dataset_split['dev']['X'], label=dataset_split['dev']['y'])
+    test_X, test_y = dataset_split['test']['X'], dataset_split['test']['y']
+    
+    params = asdict(training_config)
+    params['verbose'] = -1
+    params['random_state'] = seed
+    model = lgb.train(params, train_set=train_data, valid_sets=[dev_data], callbacks=[lgb.early_stopping(stopping_rounds=5)])
+    predictions = model.predict(test_X)
+    roc_auc = roc_auc_score(test_y, predictions)
+    return roc_auc
+    #model = lgb.train(params, train_set=train_data, valid_sets=[(dataset_split['dev']['X'], dataset_split['dev']['y'])])
     
     
-    
-    
-    #dataset = sklearn.datasets.load_breast_cancer()
-    #X = dataset.data
-    #y = dataset.target
-    
+def process_work(work_package):
+    roc_auc = train_model(work_package['split'], work_package['training_config'], work_package['seed'])
+    return roc_auc
+
 def main():
     training_config = TrainingConfig()
     
     X,y = load_data()
     
     rng = np.random.default_rng(SEED)
+    work_packages = []
     for split in make_splits(X,y, rng):
-        train_model(split, training_config)
+        work_package = {'split': split, 'seed': rng.integers(0, 2**32-1), 'training_config': training_config}
+        work_packages.append(work_package)
+    
+    performance_distribution = []
+    for work_package in tqdm(work_packages, 'Work packages'):
+        performance = process_work(work_package)    
+        performance_distribution.append(performance)
+    
+    alpha = 0.05
+    phi_1, phi_2 = alpha/2, 1-alpha/2
+    ci_low, median, ci_high = np.quantile(performance_distribution, (phi_1, 0.5, phi_2))
+    print(f"Cross validated ROC AUC: {median} ({1-alpha:%}CI [{ci_low},{ci_high}])")
+    
     
 
 if __name__ == '__main__':
